c3bc3151a7b408c0dc2fe4c769fdef402123b1a1
DattatreyaReddy <dattatreyareddypanta2001@gmail.com>
2020-08-20 12:22:28 +0530

    'Added back up option'

--- start.sh
+++ start.sh
@@ -1,4 +1,29 @@
-echo Installing dependencies...
-pip3 install -r requirements.txt 
-echo initialising the bot...
+echo "Installing dependencies..."
+pip3 install -r requirements.txt
+echo "Checking  Backup files"
+if [ -d "database" ]
+then
+    echo "Database folder found"
+else
+    mkdir "database"
+    echo "Database folder created"
+fi
+if [ -f "dbbackup/teleBot.sqlite" ]
+then
+    rm "database/teleBot.sqlite"
+    cp "dbbackup/teleBot.sqlite" "database/teleBot.sqlite"
+    echo "Backup Restored"
+elif [ -f "database/teleBot.sqlite" ]
+then
+    mkdir "dbbackup"
+    echo "Backup folder created"
+    cp "database/teleBot.sqlite" "dbbackup/teleBot.sqlite"
+    echo "Present Database Backup completed"
+else 
+    mkdir "dbbackup"
+    echo "Created Database Backup folder"
+fi
+echo "initialising the bot..."
 python pyFiles/CR_ALT.py
+cp "database/teleBot.sqlite" "dbbackup/teleBot.sqlite"
+echo "Database Backup completed"
